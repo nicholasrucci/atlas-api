756ac1ac29c045444cee5248dbbf6a12a78e0737
Nick Rucci <ld.tech@outlook.com>
2016-04-30 02:07:45 -0600

    add migrations

---
+++ db/db.go
@@ -0,0 +1,23 @@
+package db
+
+import (
+	"atlas-api/config/schema"
+
+	"github.com/jinzhu/gorm"
+
+	// dialect for gorm
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+)
+
+// Connection will open the applications database and return
+// it and a possible error
+func Connection() (*gorm.DB, error) {
+	db, err := gorm.Open("mysql", "root:@/atlas?charset=utf8&parseTime=True&loc=Local")
+	return db, err
+}
+
+// Migrate will go through each of the tables and migrate
+// them if they have not yet been migrated
+func Migrate(db *gorm.DB) {
+	db.AutoMigrate(&schema.User{})
+}
---
+++ db/migrations/20160430020410_AddSome.go
@@ -0,0 +1,14 @@
+package db
+
+import (
+	"database/sql"
+)
+
+// Up is executed when this migration is applied
+func Up_20160430020410(txn *sql.Tx) {
+}
+
+// Down is executed when this migration is rolled back
+func Down_20160430020410(txn *sql.Tx) {
+
+}
