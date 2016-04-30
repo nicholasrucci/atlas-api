dfe199db63e366e859e3115fd03c70be49e49d29
Nick Rucci <ld.tech@outlook.com>
2016-04-30 00:52:28 -0600

    Update to use struct

--- middleware/authCredentials.go
+++ middleware/authCredentials.go
@@ -4,10 +4,16 @@ import (
 	"crypto/rand"
 	"encoding/hex"
 	"io"
-	"log"
 
 	"golang.org/x/crypto/scrypt"
 )
+
+// Credentials is a struct that will be returned from the methods below
+// containing the salt and hash of a password
+type Credentials struct {
+	salt string
+	hash string
+}
 
 var (
 	// PasswordSaltBytes is the number of bytes a salt will be
@@ -19,17 +25,21 @@ var (
 
 // CreateCredentials will hash the password and return the hash
 // and the salt used when creating a user
-func CreateCredentials(password string) (string, string, error) {
+func CreateCredentials(password string) (Credentials, error) {
+	var credentials Credentials
 	salt := make([]byte, PasswordSaltBytes)
 	_, err := io.ReadFull(rand.Reader, salt)
 	if err != nil {
-		return "", "", err
+		return credentials, err
 	}
 
 	hash, err := scrypt.Key([]byte(password), salt, 1<<14, 8, 1, PasswordHashBytes)
 	if err != nil {
-		return "", "", err
+		return credentials, err
 	}
 
-	return hex.EncodeToString(salt), hex.EncodeToString(hash), err
+	credentials.hash = hex.EncodeToString(hash)
+	credentials.salt = hex.EncodeToString(salt)
+
+	return credentials, err
 }
