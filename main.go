660f45b54e3a091171d83426efd665d0b2db99bf
Nick Rucci <ld.tech@outlook.com>
2016-04-29 22:45:07 -0600

    Handle database

--- main.go
+++ main.go
@@ -5,11 +5,17 @@ import (
 	"log"
 
 	"atlas-api/route"
+	"atlas-api/config/db"
 )
 
 
 
 func main() {
+	database, err := db.Connection()
+	if err != nil {
+		log.Fatal(err)
+	}
+	db.Migrate(database)
 	router := route.NewRouter()
 	log.Fatal(http.ListenAndServe(":3000", router))
 }
