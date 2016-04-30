d1568fc9d9cebc73854b1933104288b61bb10d6f
Nick Rucci <ld.tech@outlook.com>
2016-04-29 20:31:09 -0600

    gorm set up example

--- main.go
+++ main.go
@@ -1,57 +1,55 @@
 package main
 
 import (
-	"crypto/rand"
-	"encoding/hex"
-	"fmt"
-	"io"
 	"log"
 
-	"golang.org/x/crypto/scrypt"
-)
-
-const (
-	PW_SALT_BYTES = 32
-	PW_HASH_BYTES = 64
-
-	password       = "hello"
-	other_password = "hello"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
 )
 
 type User struct {
-	hash string
-	salt string
+	gorm.Model
+	FirstName    string `gorm:"size:255"`
+	LastName     string `gorm:"size:255"`
+	Email        string `gorm:"type:varchar(100);unique"`
+	PasswordHash string `gorm:"size:255"`
+	PasswordSalt string `gorm:"size:255"`
+	Disabled     bool
 }
 
 func main() {
-
-	salt := make([]byte, PW_SALT_BYTES)
-	_, err := io.ReadFull(rand.Reader, salt)
+	db, err := gorm.Open("mysql", "root:@/atlas?charset=utf8&parseTime=True&loc=Local")
 	if err != nil {
-		log.Fatal(err)
+		log.Print(err)
 	}
 
-	texthash, err := scrypt.Key([]byte(other_password), salt, 1<<14, 8, 1, PW_HASH_BYTES)
-	if err != nil {
-		log.Fatal(err)
+	if !db.HasTable(&User{}) {
+		db.CreateTable(&User{})
 	}
+	db.Create(&User{FirstName: "Nick", Email: "none"})
 
-	hash, err := scrypt.Key([]byte(password), salt, 1<<14, 8, 1, PW_HASH_BYTES)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	user := User{
-		hash: hex.EncodeToString(hash),
-		salt: string(salt),
-	}
-
-	if user.hash == hex.EncodeToString(texthash) {
-		fmt.Println("Works")
-	} else {
-		fmt.Println("Doesn't work")
-	}
-
+	// salt := make([]byte, PW_SALT_BYTES)
+	// _, err := io.ReadFull(rand.Reader, salt)
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	//
+	// texthash, err := scrypt.Key([]byte(other_password), salt, 1<<14, 8, 1, PW_HASH_BYTES)
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	//
+	// hash, err := scrypt.Key([]byte(password), salt, 1<<14, 8, 1, PW_HASH_BYTES)
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	//
+	// if user.hash == hex.EncodeToString(texthash) {
+	// 	 fmt.Println("Works")
+	// } else {
+	// 	 fmt.Println("Doesn't work")
+	// }
+	//
 	// fmt.Println(user.hash)
 	// fmt.Print(hex.EncodeToString(texthash))
 
