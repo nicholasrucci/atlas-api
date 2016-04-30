cf8e25aa8c846213ce3d82af7c677108e6415529
Nick Rucci <ld.tech@outlook.com>
2016-04-29 23:44:54 -0600

    Add handler function for rendering json

---
+++ middleware/jsonHandler.go
@@ -0,0 +1,12 @@
+package middleware
+
+import (
+	"net/http"
+)
+
+// JSONHandler is set the header to have the content type of json
+func JSONHandler(rw http.ResponseWriter, req *http.Request) {
+	rw.Header().Set("Content-Type", "application/json; charset=UTF-8")
+	rw.Header().Set("Access-Control-Allow-Origin", "*")
+	rw.WriteHeader(http.StatusOK)
+}
