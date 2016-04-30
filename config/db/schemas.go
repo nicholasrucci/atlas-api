aad92d2d9e6346bdfa0d34f6d629e7c07ea94571
Nick Rucci <ld.tech@outlook.com>
2016-04-29 22:45:42 -0600

    Add User schema

---
+++ config/db/schemas.go
@@ -0,0 +1,16 @@
+package db
+
+import(
+	"github.com/jinzhu/gorm"
+)
+
+// User schema
+type User struct {
+	gorm.Model
+	FirstName    string `gorm:"size:255"`
+	LastName     string `gorm:"size:255"`
+	Email        string `gorm:"type:varchar(100);unique"`
+	PasswordHash string `gorm:"size:255"`
+	PasswordSalt string `gorm:"size:255"`
+	Disabled     bool
+}
