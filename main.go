73debc623a18787fd9d9cf75d9501dbd664c4813
Nick Rucci <ld.tech@outlook.com>
2016-04-29 20:51:05 -0600

    Use atlas-api/route package

--- main.go
+++ main.go
@@ -3,10 +3,13 @@ package main
 import (
 	"net/http"
 	"log"
+
+	"atlas-api/route"
 )
 
 
 
 func main() {
-	log.Fatal(http.ListenAndServe(":3000", nil))
+	router := route.NewRouter()
+	log.Fatal(http.ListenAndServe(":3000", router))
 }
