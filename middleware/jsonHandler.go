b32ce8c3a9e4b0de3663236114eec8ed7e9492f5
Nick Rucci <ld.tech@outlook.com>
2016-05-03 10:48:13 -0600

    make json handler more usable in other places

--- middleware/jsonHandler.go
+++ middleware/jsonHandler.go
@@ -7,6 +7,4 @@ import (
 // JSONHandler is set the header to have the content type of json
 func JSONHandler(rw http.ResponseWriter, req *http.Request) {
 	rw.Header().Set("Content-Type", "application/json; charset=UTF-8")
-	rw.Header().Set("Access-Control-Allow-Origin", "*")
-	rw.WriteHeader(http.StatusOK)
 }
