56d9ba4405744ed0fcecc762c9878846cea12b90
Nick Rucci <ld.tech@outlook.com>
2016-05-05 15:48:14 -0600

    use new database

--- main.go
+++ main.go
@@ -4,19 +4,11 @@ import (
 	"log"
 	"net/http"
 
-	"atlas-api/config/db"
-	"atlas-api/config/schema"
+	_ "atlas-api/config/db"
 	"atlas-api/route"
 )
 
 func main() {
-	err := db.InitializeConnection()
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	db.DB.AutoMigrate(&schema.User{}, &schema.Organization{}, &schema.Project{}, &schema.Page{}, &schema.Platform{}, &schema.Task{}, &schema.Group{})
-
 	router := route.NewRouter()
 
 	log.Fatal(http.ListenAndServe(":3000", router))
