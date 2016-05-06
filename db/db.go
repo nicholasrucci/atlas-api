708cf65e037500e4e115d6e711fd003d0d66eb1c
Nick Rucci <ld.tech@outlook.com>
2016-05-06 16:25:57 -0600

    Set up database

---
+++ db/db.go
@@ -0,0 +1,16 @@
+package db
+
+import (
+	"database/sql"
+	"log"
+
+	_ "github.com/lib/pq"
+)
+
+func init() {
+	db, err := sql.Open("postgres", "user=nicholasrucci dbname=atlas sslmode=disable")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+}
