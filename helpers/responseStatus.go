bea5f17701bc9a5ba536fa4c25c05f8f7fed7cf5
Nick Rucci <ld.tech@outlook.com>
2016-05-07 01:28:51 -0600

    add handlers for each model to respond with content on creation

--- helpers/responseStatus.go
+++ helpers/responseStatus.go
@@ -3,16 +3,19 @@ package helper
 import (
 	"encoding/json"
 	"net/http"
+
+	"atlas-api/config/schema"
 )
 
 // JSONError will hold the data that is responded to the client
-type JSONError struct {
-	Code  int
-	Error error
+type Response struct {
+	Code     int
+	Response interface{}
+	Error    error
 }
 
-func HandleError(rw http.ResponseWriter, req *http.Request, status int, incomingError error) error {
-	responseError := JSONError{status, incomingError}
+func OrganizationResponse(rw http.ResponseWriter, req *http.Request, status int, response schema.Organization, incomingError error) error {
+	responseError := Response{status, response, incomingError}
 
 	JSONHandler(rw, req)
 
@@ -20,7 +23,37 @@ func HandleError(rw http.ResponseWriter, req *http.Request, status int, incoming
 
 	err := json.NewEncoder(rw).Encode(responseError)
 	if err != nil {
-		responseError = JSONError{500, err}
+		responseError = Response{500, response, err}
+	}
+
+	return nil
+}
+
+func UserResponse(rw http.ResponseWriter, req *http.Request, status int, response schema.User, incomingError error) error {
+	responseError := Response{status, response, incomingError}
+
+	JSONHandler(rw, req)
+
+	rw.WriteHeader(status)
+
+	err := json.NewEncoder(rw).Encode(responseError)
+	if err != nil {
+		responseError = Response{500, response, err}
+	}
+
+	return nil
+}
+
+func ProjectResponse(rw http.ResponseWriter, req *http.Request, status int, response schema.Project, incomingError error) error {
+	responseError := Response{status, response, incomingError}
+
+	JSONHandler(rw, req)
+
+	rw.WriteHeader(status)
+
+	err := json.NewEncoder(rw).Encode(responseError)
+	if err != nil {
+		responseError = Response{500, response, err}
 	}
 
 	return nil
