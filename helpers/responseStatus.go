7856e7b087d9929e5380520a980c9e04b447fc45
Nick Rucci <ld.tech@outlook.com>
2016-05-09 11:02:44 -0600

    new response handler

--- helpers/responseStatus.go
+++ helpers/responseStatus.go
@@ -3,8 +3,6 @@ package helper
 import (
 	"encoding/json"
 	"net/http"
-
-	"atlas-api/config/schema"
 )
 
 // JSONError will hold the data that is responded to the client
@@ -14,37 +12,7 @@ type Response struct {
 	Error    error
 }
 
-func OrganizationResponse(rw http.ResponseWriter, req *http.Request, status int, response schema.Organization, incomingError error) error {
-	responseError := Response{status, response, incomingError}
-
-	JSONHandler(rw, req)
-
-	rw.WriteHeader(status)
-
-	err := json.NewEncoder(rw).Encode(responseError)
-	if err != nil {
-		responseError = Response{500, response, err}
-	}
-
-	return nil
-}
-
-func UserResponse(rw http.ResponseWriter, req *http.Request, status int, response schema.User, incomingError error) error {
-	responseError := Response{status, response, incomingError}
-
-	JSONHandler(rw, req)
-
-	rw.WriteHeader(status)
-
-	err := json.NewEncoder(rw).Encode(responseError)
-	if err != nil {
-		responseError = Response{500, response, err}
-	}
-
-	return nil
-}
-
-func ProjectResponse(rw http.ResponseWriter, req *http.Request, status int, response schema.Project, incomingError error) error {
+func CreateResponse(rw http.ResponseWriter, req *http.Request, status int, response interface{}, incomingError error) error {
 	responseError := Response{status, response, incomingError}
 
 	JSONHandler(rw, req)
